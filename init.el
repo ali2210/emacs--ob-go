b7b6f0a45a6f79c045d46af050cc210e31b0e874
Ali Hassan <20568079+ali2210@users.noreply.github.com>
2018-12-09 23:15:15 +0500

    split-window added

---
+++ init.el
@@ -0,0 +1,78 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require  'package)
+(add-to-list 'package-archives'
+  ("melpa" . "http://melpa.milkbox.net/packages/") t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(package-selected-packages
+   (quote
+    (ob-go org-babel-eval-in-repl ob-async spacemacs-theme neotree flymake-go go-autocomplete auto-complete go-gopath go-projectile exec-path-from-shell go-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-to-list 'load-path"~/emacs.d/")
+
+(when(memq window-system '(mac ns))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
+
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq gofmt-command "goimports")
+  (if(not(string-match "go" compile-command))
+      (set(make-local-variable 'compile-command)
+	  "go build -v && go test-v && go vet"))
+
+  (go-guru-h1-identifier-mode)
+  
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "M-p") 'compile)
+  (local-set-key (kbd "M-P") 'recompile)
+  (local-set-key (kbd "M-]") 'next-error)
+  (local-set-key (kbd "M-[") 'prev-error)
+  (auto-complete-mode 1))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+
+
+(with-eval-after-load 'go-mode
+(require 'go-autocomplete))
+
+
+(require 'go-guru)
+
+(require 'ob-go) 
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((go . t)))
+
+
+(defun vsplit-last-buffer ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+
+(defun hsplit-last-buffer ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+
+(global-set-key (kbd "C-x-2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-X-3") 'hsplit-last-buffer)
+	       
